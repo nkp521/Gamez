2dc9cc7a9ed2c9fe74b2f211c4654279532d72a8
Nihar Patel <niharpatel52@gmail.com>
2025-06-30 15:20:09 -0400

    Set state to toggle game state from Play Game Button

--- src/components/CameCard.js
+++ src/components/CameCard.js
@@ -3,8 +3,12 @@ import React, { useState } from "react";
 const CameCard = ({ game }) => {
   const { description, embed, image, tags, title } = game;
   const [showDescription, setShowDescription] = useState(false);
+  const [showIframe, setShowIframe] = useState(false);
 
-  const descriptionToggle = () => setShowDescription(!showDescription);
+  const toggleDescription = () => setShowDescription(!showDescription);
+  const toggleGame = () => setShowIframe(!showIframe);
+
+  console.log(showIframe, game);
 
   return (
     <div className="w-[364px] border rounded p-4">
@@ -16,7 +20,13 @@ const CameCard = ({ game }) => {
       <h2 className="text-lg font-semibold mb-1 text-center">{title}</h2>
       <button
         className="bg-blue-500 text-white text-sm px-3 py-1 rounded mx-auto block mt-2 hover:bg-blue-600 transition"
-        onClick={descriptionToggle}
+        onClick={toggleGame}
+      >
+        Play Game
+      </button>
+      <button
+        className="bg-blue-500 text-white text-sm px-3 py-1 rounded mx-auto block mt-2 hover:bg-blue-600 transition"
+        onClick={toggleDescription}
       >
         Game Details
       </button>
