b29179434ee1a6444a74931ab68403c37c46d292
Nihar Patel <niharpatel52@gmail.com>
2025-06-30 11:51:20 -0400

    Initialize Tailwinds.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,13 @@ import React from "react";
 
 const Header = () => {
   return (
-    <>
-      <h3>Home</h3>
-      <h3>Favorite Games</h3>
-      <h3>Submit Games</h3>
-    </>
+    <nav>
+      <div>
+        <h3>Home</h3>
+        <h3>Favorite Games</h3>
+        <h3>Submit Games</h3>
+      </div>
+    </nav>
   );
 };
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Header from "./Header";
+
 const Home = () => {
   // api="https://www.onlinegames.io/media/plugins/genGames/embed.json"
   return (
     <div>
       <header>
         <h1>Welcome to Gamez</h1>
+        <Header />
       </header>
     </div>
   );
