8dad21288f538db6118ee3d1db7a288201edbf70
Nihar Patel <niharpatel52@gmail.com>
2025-06-30 15:05:41 -0400

    Add button to GameCard to toggle game description

--- src/components/CameCard.js
+++ src/components/CameCard.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CameCard = ({ game }) => {
   const { description, embed, image, tags, title } = game;
+  const [showDescription, setShowDescription] = useState(false);
+
+  const descriptionToggle = () => setShowDescription(!showDescription);
 
   return (
     <div className="w-[364px] h-[384px] border rounded p-4">
@@ -10,8 +13,9 @@ const CameCard = ({ game }) => {
         alt={title}
         className="w-full h-[192px] object-cover rounded"
       />
-      <h2>{title}</h2>
-      <p>{description}</p>
+      <h2 className="text-lg font-semibold mb-1">{title}</h2>
+      <button onClick={descriptionToggle}>Game Details</button>
+      {showDescription && <p>{description}</p>}
     </div>
   );
 };
