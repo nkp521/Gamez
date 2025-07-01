129e71f484b1a2ae08b93cb985aebdde6d7bee77
Nihar Patel <niharpatel52@gmail.com>
2025-07-01 08:20:48 -0400

    Add initial tailwind form from Mamba UI

--- src/pages/SubmitGames.js
+++ src/pages/SubmitGames.js
@@ -1,7 +1,56 @@
 const SubmitGames = () => {
   return (
-    <div>
-      <h1>Submit Games</h1>
+    <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-100 dark:text-gray-800">
+      <div className="flex flex-col justify-between">
+        <div className="space-y-2">
+          <h2 className="text-4xl font-bold leading-tight lg:text-5xl">
+            Form Details
+          </h2>
+          <div className="dark:text-gray-600 font-bold">
+            Detailed Submission Guide
+          </div>
+        </div>
+        {/* <img src="assets/svg/doodle.svg" alt="" className="p-6 h-52 md:h-64" /> */}
+      </div>
+      <form noValidate="" className="space-y-6">
+        <div>
+          <label htmlFor="name" className="text-sm">
+            Game Name
+          </label>
+          <input
+            id="name"
+            type="text"
+            placeholder=""
+            className="w-full p-3 rounded dark:bg-gray-100"
+          />
+        </div>
+        <div>
+          <label htmlFor="email" className="text-sm">
+            Game Link
+          </label>
+          <input
+            id="email"
+            type="email"
+            className="w-full p-3 rounded dark:bg-gray-100"
+          />
+        </div>
+        <div>
+          <label htmlFor="message" className="text-sm">
+            Game Description
+          </label>
+          <textarea
+            id="message"
+            rows="3"
+            className="w-full p-3 rounded dark:bg-gray-100"
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          className="w-full p-3 text-sm font-bold tracking-wide uppercase rounded dark:bg-violet-600 dark:text-gray-50"
+        >
+          Submit Game
+        </button>
+      </form>
     </div>
   );
 };
