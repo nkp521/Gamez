4f7e6fe183d08e4e82a2cf39596eac590e75668b
Nihar Patel <niharpatel52@gmail.com>
2025-09-16 08:18:46 -0400

    Add tsx dependencies

---
+++ src/declarations.d.ts
@@ -0,0 +1,12 @@
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.svg" {
+  import * as React from "react";
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+declare module "*.css";
+declare module "*.module.css";
